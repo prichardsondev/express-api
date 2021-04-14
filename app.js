35bc8a72491b6409fea09dc61d34dc3023913f0f
prichardsondev <prichardsondev@gmail.com>
2021-04-14 12:42:09 -0400

    add express server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+app.get("/add", (req, res) => {
+    // let v1 = req.query.v1;
+    // let v2 = req.query.v2;
+
+    let {v1:bunnies,v2} = req.query;
+
+    let sum = parseInt(v1) + parseInt(v2);
+    res.json(sum);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
